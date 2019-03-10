3c1f98aca1dad16af87c31905bae6c4438971354
Natalie Halawani <hala1224@gmail.com>
2019-03-10 00:38:46 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  configure do 
+    enable :sessions
+    set :session_secret, "Natasha"
+  end
+  
+  get '/' do 
+    @session = session 
+    erb :index
+  end
+  
+  post '/checkout'
+    erb :index
+  end
+  
 end
